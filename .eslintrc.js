92e55bbbeb45b93cce4a7286b9b2445e05d34e86
Hans Andersen <hans@enterprisedt.com>
2018-10-22 10:53:05 +1000

    Update eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
-    rules: {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "jquery": true
+    },
+      rules: {
         'no-console': 'off',
         'no-unused-vars': 'off'
     },
